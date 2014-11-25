2954136e2cd1edc005a0b8cb228d60249c0687b1
Philippe Marschall <philippe.marschall@gmail.com>
2014-11-25 18:20:01 +0100

    jndi

--- dagger-example-web/src/main/java/com/github/marschall/dagger/web/WebModule.java
+++ dagger-example-web/src/main/java/com/github/marschall/dagger/web/WebModule.java
@@ -1,15 +1,10 @@
 package com.github.marschall.dagger.web;
 
-import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.SQLFeatureNotSupportedException;
-import java.util.logging.Logger;
-
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 import com.github.marschall.dagger.bricks.jdbc.TransactionModule;
-import com.github.marschall.dagger.service.DefaultInfostoreService;
 import com.github.marschall.dagger.service.InfostoreModule;
 
 import dagger.Module;
@@ -21,67 +16,9 @@ import dagger.Provides;
 public class WebModule {
   
   @Provides
-  public DataSource provideDataSource() {
-    return new NullDataSource();
+  public DataSource provideDataSource() throws NamingException {
+    InitialContext initialContext = new InitialContext();
+    return (DataSource) initialContext.lookup("jdbc/dagger");
   }
   
-  static final class NullDataSource implements DataSource {
-
-    @Override
-    public PrintWriter getLogWriter() throws SQLException {
-      // TODO Auto-generated method stub
-      return null;
-    }
-
-    @Override
-    public void setLogWriter(PrintWriter out) throws SQLException {
-      // TODO Auto-generated method stub
-      
-    }
-
-    @Override
-    public void setLoginTimeout(int seconds) throws SQLException {
-      // TODO Auto-generated method stub
-      
-    }
-
-    @Override
-    public int getLoginTimeout() throws SQLException {
-      // TODO Auto-generated method stub
-      return 0;
-    }
-
-    @Override
-    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
-      // TODO Auto-generated method stub
-      return null;
-    }
-
-    @Override
-    public <T> T unwrap(Class<T> iface) throws SQLException {
-      // TODO Auto-generated method stub
-      return null;
-    }
-
-    @Override
-    public boolean isWrapperFor(Class<?> iface) throws SQLException {
-      // TODO Auto-generated method stub
-      return false;
-    }
-
-    @Override
-    public Connection getConnection() throws SQLException {
-      // TODO Auto-generated method stub
-      return null;
-    }
-
-    @Override
-    public Connection getConnection(String username, String password)
-        throws SQLException {
-      // TODO Auto-generated method stub
-      return null;
-    }
-    
-  }
-
 }
